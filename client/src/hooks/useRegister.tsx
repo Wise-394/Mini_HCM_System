59323d60f9e5667600a81106c1a7b3e74283f0b5
wise <114738222+Wise-394@users.noreply.github.com>
2026-06-16 22:11:50 +0800

    refactor: update registration logic to use UserProfileType and streamline data handling

--- client/src/hooks/useRegister.tsx
+++ client/src/hooks/useRegister.tsx
@@ -2,19 +2,11 @@ import { useMutation } from '@tanstack/react-query';
 import { createUserWithEmailAndPassword, getIdToken } from 'firebase/auth';
 import type { User } from 'firebase/auth';
 import { auth } from '../configs/firebase.ts';
-
-interface RegistrationData {
-  name: string;
-  email: string;
-  timezone: string;
-  schedule_start: string;
-  schedule_end: string;
-}
+import type { UserProfileType } from '../types/types.ts';
 
 interface RegisterVariables {
-  email: string;
   password: string;
-  profileData: RegistrationData;
+  userProfile: Omit<UserProfileType, 'uid'>;
 }
 
 //1.Register the user on firebase auth
@@ -22,10 +14,10 @@ interface RegisterVariables {
 //3.express will save user data to collection
 export const useRegister = () => {
   const mutation = useMutation<User, Error, RegisterVariables>({
-    mutationFn: async ({ email, password, profileData }) => {
+    mutationFn: async ({ password, userProfile }) => {
       const userCredential = await createUserWithEmailAndPassword(
         auth,
-        email,
+        userProfile.email,
         password
       );
 
@@ -38,7 +30,7 @@ export const useRegister = () => {
         },
         body: JSON.stringify({
           uid: userCredential.user.uid,
-          ...profileData,
+          ...userProfile,
         }),
       });
 
--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,14 +1,15 @@
-export type UserRole = "employee" | "admin";
+export type UserRole = 'employee' | 'admin';
 
 export interface WorkSchedule {
   start: string;
   end: string;
 }
 
-export interface UserProfile {
+export interface UserProfileType {
+  uid: string | null;
   name: string;
   email: string;
-  role: UserRole;
+  role: UserRole | null;
   timezone: string;
   schedule: WorkSchedule;
 }
